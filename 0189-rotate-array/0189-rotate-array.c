23b8cb26883274013ab3f15ff48fad903559079b
Arnav Bhatnagar <arnavbhatnagar2007@gmail.com>
2026-02-16 23:41:53 +0530

    Time: 0 ms (100%), Space: 27.3 MB (7.91%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.c
@@ -0,0 +1,12 @@
+void rotate(int* nums, int numsSize, int k) {
+    if (numsSize == 0) return;
+    k = k % numsSize; 
+    int* temp = (int*)malloc(numsSize * sizeof(int));
+    for (int i = 0; i < numsSize; i++) {
+        temp[(i + k) % numsSize] = nums[i];
+    }
+    for (int i = 0; i < numsSize; i++) {
+        nums[i] = temp[i];
+    }
+    free(temp);
+}
