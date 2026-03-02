33728d3b3d06a4f653faab5cce261193678be7f3
Arnav Bhatnagar <arnavbhatnagar2007@gmail.com>
2026-03-02 19:42:48 +0530

    Time: 3 ms (48.63%), Space: 45.4 MB (19.36%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.c
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* reverseList(struct ListNode* head) {
+    struct ListNode* prev = NULL;
+    struct ListNode* curr = head;
+    while (curr != NULL) {
+        struct ListNode* nextTemp = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = nextTemp;
+    }
+    return prev;
+}
+
+bool isPalindrome(struct ListNode* head) {
+    if (head == NULL || head->next == NULL) return true;
+    struct ListNode* slow = head;
+    struct ListNode* fast = head;
+    while (fast->next != NULL && fast->next->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    struct ListNode* secondHalf = reverseList(slow->next);
+    struct ListNode* p1 = head;
+    struct ListNode* p2 = secondHalf;
+    bool result = true;
+    while (p2 != NULL) {
+        if (p1->val != p2->val) {
+            result = false;
+            break;
+        }
+        p1 = p1->next;
+        p2 = p2->next;
+    }
+    slow->next = reverseList(secondHalf);
+
+    return result;
+}
+struct ListNode* newNode(int val) {
+    struct ListNode* node = (struct ListNode*)malloc(sizeof(struct ListNode));
+    node->val = val;
+    node->next = NULL;
+    return node;
+}
