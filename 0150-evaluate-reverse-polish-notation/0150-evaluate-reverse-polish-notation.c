0def2bddc87e18f2e72afcdf5289198227783a67
Arnav Bhatnagar <arnavbhatnagar2007@gmail.com>
2026-03-09 22:20:36 +0530

    Time: 0 ms (100%), Space: 10.3 MB (32.35%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.c
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.c
@@ -1,26 +1,17 @@
 int evalRPN(char **tokens, int tokensSize) {
     int stack[tokensSize];
     int top = -1;
-
     for (int i = 0; i < tokensSize; i++) {
         char *t = tokens[i];
-
-        if (!strcmp(t, "+")) {
+        if ((t[1] == '\0') && (t[0] == '+' || t[0] == '-' || t[0] == '*' || t[0] == '/')) {
             int b = stack[top--];
             int a = stack[top--];
-            stack[++top] = a + b;
-        } else if (!strcmp(t, "-")) {
-            int b = stack[top--];
-            int a = stack[top--];
-            stack[++top] = a - b;
-        } else if (!strcmp(t, "*")) {
-            int b = stack[top--];
-            int a = stack[top--];
-            stack[++top] = a * b;
-        } else if (!strcmp(t, "/")) {
-            int b = stack[top--];
-            int a = stack[top--];
-            stack[++top] = a / b;  
+            switch (t[0]) {
+                case '+': stack[++top] = a + b; break;
+                case '-': stack[++top] = a - b; break;
+                case '*': stack[++top] = a * b; break;
+                case '/': stack[++top] = a / b; break; 
+            }
         } else {
             stack[++top] = atoi(t);  
         }
