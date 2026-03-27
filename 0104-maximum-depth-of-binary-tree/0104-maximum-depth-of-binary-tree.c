07b5f0649bc571c27565768cc10afc2ca6709339
Arnav Bhatnagar <arnavbhatnagar2007@gmail.com>
2026-03-27 18:42:44 +0530

    Time: 0 ms (100%), Space: 11.9 MB (48.13%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.c
@@ -0,0 +1,16 @@
+struct TreeNode* newNode(int val) {
+    struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    node->val = val;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+
+int maxDepth(struct TreeNode* root) {
+    if (root == NULL) {
+        return 0;
+    }
+    int leftDepth = maxDepth(root->left);
+    int rightDepth = maxDepth(root->right);
+    return (leftDepth > rightDepth ? leftDepth : rightDepth) + 1;
+}
