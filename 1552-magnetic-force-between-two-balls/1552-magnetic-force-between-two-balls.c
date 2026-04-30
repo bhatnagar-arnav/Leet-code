ed0e7f2c57d26c6e7c578888a36454762de040c7
Arnav Bhatnagar <arnavbhatnagar2007@gmail.com>
2026-04-30 23:51:14 +0530

    Time: 87 ms (27.27%), Space: 18.2 MB (45.45%) - LeetHub

---
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.c
@@ -0,0 +1,30 @@
+int cmp(const void* a, const void* b){
+    return (*(int*)a - *(int*)b);
+}
+
+int canPlace(int* pos, int n, int m, int dist){
+    int count = 1, last = pos[0];
+    for(int i = 1; i < n; i++){
+        if(pos[i] - last >= dist){
+            count++;
+            last = pos[i];
+            if(count >= m) return 1;
+        }
+    }
+    return 0;
+}
+
+int maxDistance(int* position, int positionSize, int m){
+    qsort(position, positionSize, sizeof(int), cmp);
+    int low = 1, high = position[positionSize-1] - position[0], ans = 0;
+    while(low <= high){
+        int mid = low + (high - low) / 2;
+        if(canPlace(position, positionSize, m, mid)){
+            ans = mid;
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    return ans;
+}
